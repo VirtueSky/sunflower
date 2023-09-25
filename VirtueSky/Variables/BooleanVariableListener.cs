5311345abb40822641e490b73ae21ecaed5b647d
Virtuesky <virtuesky.0520@gmail.com>
2023-09-25 21:44:31 +0700

    add bool variable listener

---
+++ VirtueSky/Variables/BooleanVariableListener.cs
@@ -0,0 +1,8 @@
+using VirtueSky.Events;
+
+namespace VirtueSky.Variables
+{
+    public class BooleanVariableListener : BaseVariableListener<bool, BooleanVariable, BooleanEventResponse>
+    {
+    }
+}
