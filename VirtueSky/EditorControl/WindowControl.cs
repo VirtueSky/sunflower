18fb6a389aa23ad78128bc1aef9490e91404cf86
Virtuesky <virtuesky.0520@gmail.com>
2023-10-07 20:39:38 +0700

    add create scriptableobject by window control

--- VirtueSky/EditorControl/WindowControl.cs
+++ VirtueSky/EditorControl/WindowControl.cs
@@ -1,12 +1,18 @@
 using UnityEditor;
 using UnityEngine;
 using VirtueSky.DataStorage;
+using VirtueSky.EditorUtils;
+using VirtueSky.Events;
+using VirtueSky.ObjectPooling;
+using VirtueSky.Variables;
 
 namespace VirtueSky.EditorControl
 {
 #if UNITY_EDITOR
     public class WindowControl : EditorWindow
     {
+        #region Data
+
         [MenuItem("Sunflower/Clear Data %F1")]
         public static void ClearAllData()
         {
@@ -21,6 +27,145 @@ namespace VirtueSky.EditorControl
         {
             GameData.Save();
         }
+
+        #endregion
+
+        #region Create ScriptableObject Event
+
+        private const string pathEvent = "/Event";
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/EventNoParam")]
+        public static void CreateEventNoParam()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<EventNoParam>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/String Event")]
+        public static void CreateEventString()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<StringEvent>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/Float Event")]
+        public static void CreateEventFloat()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<FloatEvent>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/Integer Event")]
+        public static void CreateEventInt()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<IntegerEvent>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/Boolean Event")]
+        public static void CreateEventBoolean()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<BooleanEvent>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/Object Event")]
+        public static void CreateEventObject()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<ObjectEvent>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/Play Audio Event")]
+        public static void CreateEventPlayAudio()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<PlayAudioEvent>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/Short Double Event")]
+        public static void CreateEventShortDouble()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<ShortDoubleEvent>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/Dictionary Event")]
+        public static void CreateEventDictionary()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<DictionaryEvent>(pathEvent);
+        }
+
+        [MenuItem("Sunflower/Create ScriptableObject Event/Vector3 Event")]
+        public static void CreateEventVector3()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<Vector3Event>(pathEvent);
+        }
+
+        #endregion
+
+        #region Create ScriptableObject Variable
+
+        private const string pathVariable = "/Variable";
+        private const string menuVariable = "Sunflower/Create ScriptableObject Variable/";
+
+        [MenuItem(menuVariable + "Transform Variable")]
+        public static void CreateVariableTransform()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<TransformVariable>(pathVariable);
+        }
+
+        [MenuItem(menuVariable + "Rect Variable")]
+        public static void CreateVariableRect()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<RectVariable>(pathVariable);
+        }
+
+        [MenuItem(menuVariable + "Object Variable")]
+        public static void CreateVariableObject()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<ObjectVariable>(pathVariable);
+        }
+
+        [MenuItem(menuVariable + "Boolean Variable")]
+        public static void CreateVariableBoolean()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<BooleanVariable>(pathVariable);
+        }
+
+        [MenuItem(menuVariable + "Short Double Variable")]
+        public static void CreateVariableShortDouble()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<ShortDoubleVariable>(pathVariable);
+        }
+
+        [MenuItem(menuVariable + "Vector3 Variable")]
+        public static void CreateVariableVector3()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<Vector3Variable>(pathVariable);
+        }
+
+        [MenuItem(menuVariable + "String Variable")]
+        public static void CreateVariableString()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<StringVariable>(pathVariable);
+        }
+
+        [MenuItem(menuVariable + "Float Variable")]
+        public static void CreateVariableFloat()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<FloatVariable>(pathVariable);
+        }
+
+        [MenuItem(menuVariable + "Integer Variable")]
+        public static void CreateVariableInt()
+        {
+            ScriptableSetting.CreateScriptableAssetsOnlyName<IntegerVariable>(pathVariable);
+        }
+
+        #endregion
+
+        #region Create ScriptableObject Pools
+
+        [MenuItem("Sunflower/Create ScriptableObject Pools")]
+        public static void CreatePools()
+        {
+            ScriptableSetting.CreateScriptableAssets<Pools>("/Pools");
+        }
+
+        #endregion
     }
 #endif
 }
--- VirtueSky/EditorUtils/ScriptableSetting.cs
+++ VirtueSky/EditorUtils/ScriptableSetting.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using VirtueSky.Utils;
 
@@ -9,22 +10,50 @@ namespace VirtueSky.EditorUtils
     public static class ScriptableSetting
     {
 #if UNITY_EDITOR
-        public static void CreateSettingAssets<T>(string path = "") where T : ScriptableObject
+        public static void CreateScriptableAssets<T>(string path = "", bool isOnlyNameWithTime = false)
+            where T : ScriptableObject
         {
+            string name = isOnlyNameWithTime == false
+                ? typeof(T).Name
+                : $"{typeof(T).Name}_{DateTime.Now:yyyyMMddHHmmss}";
             var setting = UnityEngine.ScriptableObject.CreateInstance<T>();
-            UnityEditor.AssetDatabase.CreateAsset(setting, $"{DefaultResourcesPath(path)}/{typeof(T).Name}.asset");
+            UnityEditor.AssetDatabase.CreateAsset(setting, $"{DefaultResourcesPath(path)}/{name}.asset");
             UnityEditor.AssetDatabase.SaveAssets();
             UnityEditor.AssetDatabase.Refresh();
 
-            Debug.Log($"{typeof(T).Name} was created ad {DefaultResourcesPath(path)}/{typeof(T).Name}.asset");
+            Debug.Log(
+                $"<color=Green>{name} was created ad {DefaultResourcesPath(path)}/{name}.asset</color>");
         }
+
+        public static void CreateScriptableAssetsOnlyName<T>(string path = "") where T : ScriptableObject
+        {
+            int assetCounter = 0;
+            string assetName = $"{typeof(T).Name}";
+            string assetPath = $"{DefaultResourcesPath(path)}/{assetName}.asset";
+
+            while (AssetDatabase.LoadAssetAtPath<T>(assetPath) != null)
+            {
+                assetCounter++;
+                assetPath = $"{DefaultResourcesPath(path)}/{assetName} {assetCounter}.asset";
+            }
+
+            var setting = ScriptableObject.CreateInstance<T>();
+
+            UnityEditor.AssetDatabase.CreateAsset(setting, assetPath);
+            UnityEditor.AssetDatabase.SaveAssets();
+            UnityEditor.AssetDatabase.Refresh();
+
+            Debug.Log(
+                $"<color=Green>{typeof(T).Name} was created at {assetPath}</color>");
+        }
+
 
         public static T CreateAndGetScriptableAsset<T>(string path = "") where T : ScriptableObject
         {
             var so = AssetUtils.FindAssetAtFolder<T>(new string[] { "Assets" }).FirstOrDefault();
             if (so == null)
             {
-                ScriptableSetting.CreateSettingAssets<T>(path);
+                ScriptableSetting.CreateScriptableAssets<T>(path);
                 so = AssetUtils.FindAssetAtFolder<T>(new string[] { "Assets" }).FirstOrDefault();
             }
 
@@ -35,7 +64,7 @@ namespace VirtueSky.EditorUtils
 
         public static string DefaultResourcesPath(string path = "")
         {
-            const string defaultResourcePath = "Assets/_Root/Resources";
+            const string defaultResourcePath = "Assets/_Sunflower/Resources";
             if (!Directory.Exists(defaultResourcePath + path))
             {
                 Directory.CreateDirectory(defaultResourcePath + path);
