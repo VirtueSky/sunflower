ae06499fdb5bb7919b95650300a5f9a1c46b0b08
DucNV <ducnv.cntt0502@gmail.com>
2023-10-11 14:31:25 +0700

    add time remaining

---
+++ VirtueSky/Misc/TimeRemaining.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using VirtueSky.Core;
+
+namespace VirtueSky.Misc
+{
+    public class TimeRemaining : BaseMono
+    {
+        [HorizontalGroup("Year-Month-Day"), LabelWidth(90)] [SerializeField]
+        private int targetYear;
+
+        [HorizontalGroup("Year-Month-Day"), LabelWidth(90)] [SerializeField]
+        private int targetMonth;
+
+        [HorizontalGroup("Year-Month-Day"), LabelWidth(90)] [SerializeField]
+        private int targetDay;
+
+        [HorizontalGroup("Hour-Minute-Second"), LabelWidth(90)] [SerializeField]
+        private int targetHour;
+
+        [HorizontalGroup("Hour-Minute-Second"), LabelWidth(90)] [SerializeField]
+        private int targetMinute;
+
+        [HorizontalGroup("Hour-Minute-Second"), LabelWidth(90)] [SerializeField]
+        private int targetSecond;
+
+        private DateTime targetTime;
+
+
+        public void InitTargetTime()
+        {
+            targetTime = new DateTime(targetYear, targetMonth, targetDay, targetHour, targetMinute, targetSecond);
+        }
+
+        public TimeSpan GetTimeRemaining()
+        {
+            return (targetTime - DateTime.Now).TotalSeconds > 0 ? (targetTime - DateTime.Now) : TimeSpan.Zero;
+        }
+    }
+}
