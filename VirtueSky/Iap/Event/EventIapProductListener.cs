22d7baa4b0d1e14d384214a3714ddaeea0d3e997
DucNV <ducnv.cntt0502@gmail.com>
2023-10-17 09:45:08 +0700

    update

---
+++ VirtueSky/Iap/Event/EventIapProductListener.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.Events;
+using VirtueSky.Events;
+
+namespace VirtueSky.Iap
+{
+    public class IapProductEventListener : BaseEventListener<IapDataVariable, EventIapProduct, UnityEvent<IapDataVariable>>
+    {
+    }
+}
