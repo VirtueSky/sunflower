8b422ed1bf1f460ab85918c6ef90cc7bcbe08182
DucNV <ducnv.cntt0502@gmail.com>
2023-10-24 17:37:11 +0700

    add feat notification

---
+++ VirtueSky/Notifications/NotificationAndroid.cs
@@ -0,0 +1,63 @@
+﻿#if UNITY_ANDROID
+using System;
+using System.Collections.Generic;
+using Unity.Notifications.Android;
+
+namespace VirtueSky.Notifications
+{
+    internal static class NotificationAndroid
+    {
+        private static readonly Dictionary<string, bool> ChannelRegistered = new Dictionary<string, bool>();
+
+        private static void RegisterNotificationChannel(string identifier, string name, string description)
+        {
+            ChannelRegistered.TryGetValue(identifier, out bool registered);
+            if (registered) return;
+
+            AndroidNotificationCenter.RegisterNotificationChannel(new AndroidNotificationChannel
+            {
+                Id = identifier, Name = name, Importance = Importance.High, Description = description
+            });
+            ChannelRegistered.Add(identifier, true);
+        }
+
+        internal static void Schedule(
+            string identifier,
+            string title,
+            string text,
+            TimeSpan timeOffset,
+            string largeIcon = null,
+            string channelName = "Nova",
+            string channelDescription = "Newsletter Announcement",
+            string smallIcon = "icon_0",
+            BigPictureStyle? bigPictureStyle = null,
+            bool repeat = false)
+        {
+            RegisterNotificationChannel(identifier, channelName, channelDescription);
+
+            var notification = new AndroidNotification()
+            {
+                Title = title,
+                Text = text,
+                FireTime = DateTime.Now + timeOffset,
+                Group = identifier,
+                GroupSummary = true,
+                ShouldAutoCancel = true,
+                BigPicture = bigPictureStyle,
+            };
+
+            if (repeat) notification.RepeatInterval = timeOffset;
+
+            if (largeIcon != null) notification.LargeIcon = largeIcon;
+            if (smallIcon != null) notification.SmallIcon = smallIcon;
+
+            AndroidNotificationCenter.SendNotification(notification, identifier);
+        }
+
+        internal static void CancelAllScheduled()
+        {
+            AndroidNotificationCenter.CancelAllScheduledNotifications();
+        }
+    }
+}
+#endif
---
+++ VirtueSky/Notifications/NotificationIOS.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Notifications.iOS;
+using UnityEngine;
+
+namespace VirtueSky.Notifications
+{
+    internal static class NotificationIOS
+    {
+        // private static Dictionary<string, bool> channelRegistered = new Dictionary<string, bool>();
+        //
+        // internal static async UniTask RequestAuthorization()
+        // {
+        //     var authorizationOption = AuthorizationOption.Alert | AuthorizationOption.Badge | AuthorizationOption.Sound;
+        //     using (var req = new AuthorizationRequest(authorizationOption, true))
+        //     {
+        //         while (!req.IsFinished)
+        //         {
+        //             await UniTask.Yield();
+        //         }
+        //     }
+        // }
+        //
+        // private static async UniTask RegisterNotificationChannel(string identifier, string title, string subtitle, string body, iOSNotificationTrigger trigger)
+        // {
+        //     await RequestAuthorization();
+        //
+        //     if (new iOSNotificationSettings().AuthorizationStatus != AuthorizationStatus.Authorized)
+        //     {
+        //         Debug.LogError("IOSNotification non authorized for schedule notification");
+        //     }
+        //
+        //     var unregistered = !channelRegistered.ContainsKey(identifier) || !channelRegistered[identifier];
+        //     if (unregistered)
+        //     {
+        //         var notification = new iOSNotification()
+        //         {
+        //             Identifier = identifier,
+        //             Title = title,
+        //             Body = body,
+        //             Subtitle = subtitle,
+        //             ShowInForeground = true,
+        //             ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Badge | PresentationOption.Sound),
+        //             CategoryIdentifier = "category_a",
+        //             ThreadIdentifier = "thread1",
+        //             Trigger = trigger,
+        //         };
+        //
+        //         try
+        //         {
+        //             iOSNotificationCenter.ScheduleNotification(notification);
+        //             channelRegistered[identifier] = true;
+        //         }
+        //         catch (Exception e)
+        //         {
+        //             Debug.LogException(e);
+        //         }
+        //     }
+        // }
+        //
+        // internal static void Schedule(string identifier, string title, string subtitle, string text, TimeSpan fireTime, bool repeat)
+        // {
+        //     var interval = fireTime;
+        //     if (interval <= TimeSpan.Zero) interval = TimeSpan.FromSeconds(1);
+        //
+        //     var timeTrigger = new iOSNotificationTimeIntervalTrigger {TimeInterval = interval, Repeats = repeat};
+        //
+        //     RegisterNotificationChannel(identifier,
+        //             title,
+        //             subtitle,
+        //             text,
+        //             timeTrigger)
+        //         .Forget();
+        // }
+        //
+        // internal static void CancelAllScheduled() { iOSNotificationCenter.RemoveAllScheduledNotifications(); }
+        // internal static void ClearBadgeCounter() { iOSNotificationCenter.ApplicationBadge = 0; }
+    }
+}
