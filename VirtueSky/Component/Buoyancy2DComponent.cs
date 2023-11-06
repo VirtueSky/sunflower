19e1ee4f8810c8f580348480dd034a47421f946c
DucNV <ducnv.cntt0502@gmail.com>
2023-11-06 11:58:10 +0700

    add buoyancy component

---
+++ VirtueSky/Component/Buoyancy2DComponent.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using VirtueSky.Core;
+
+namespace VirtueSky.Component
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Buoyancy2DComponent : BaseMono
+    {
+        public Transform[] floaters;
+        public float underWaterDrag = 3f;
+        public float underWaterAngularDrag = 1f;
+        public float airDrag = 0f;
+        public float airAngularDrag = 0.05f;
+        public float floatingPower = 15f;
+        public float waterHeight = 0f;
+        public Rigidbody2D rb;
+        bool Underwater;
+
+        int floatersUnderWater;
+
+
+        // Update is called once per frame
+        public override void FixedTick()
+        {
+            base.FixedTick();
+            floatersUnderWater = 0;
+            for (int i = 0; i < floaters.Length; i++)
+            {
+                float diff = floaters[i].position.y - waterHeight;
+                if (diff < 0)
+                {
+                    rb.AddForceAtPosition(Vector3.up * floatingPower * Mathf.Abs(diff), floaters[i].position, ForceMode2D.Force);
+                    floatersUnderWater += 1;
+                    if (!Underwater)
+                    {
+                        Underwater = true;
+                        SwitchState(true);
+                    }
+                }
+            }
+
+            if (Underwater && floatersUnderWater == 0)
+            {
+                Underwater = false;
+                SwitchState(false);
+            }
+        }
+
+        void SwitchState(bool isUnderwater)
+        {
+            if (isUnderwater)
+            {
+                rb.drag = underWaterDrag;
+                rb.angularDrag = underWaterAngularDrag;
+            }
+            else
+            {
+                rb.drag = airDrag;
+                rb.angularDrag = airAngularDrag;
+            }
+        }
+#if UNITY_EDITOR
+        private void Reset()
+        {
+            if (rb == null)
+            {
+                rb = GetComponent<Rigidbody2D>();
+            }
+        }
+#endif
+    }
+}
