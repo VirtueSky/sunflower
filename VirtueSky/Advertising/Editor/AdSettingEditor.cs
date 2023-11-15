cffdc657974ef488cea459ac91c626c949386a36
DucNV <ducnv.cntt0502@gmail.com>
2023-11-15 14:15:25 +0700

    add adsetting editor

---
+++ VirtueSky/Advertising/Editor/AdSettingEditor.cs
@@ -0,0 +1,119 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace VirtueSky.Ads
+{
+    [CustomEditor(typeof(AdSetting), true)]
+    public class AdSettingEditor : Editor
+    {
+        private AdSetting _adSetting;
+        private SerializedProperty _adNetwork;
+        private SerializedProperty _adCheckingInterval;
+        private SerializedProperty _adLoadingInterval;
+
+        private SerializedProperty _sdkKey;
+        private SerializedProperty _applovinEnableAgeRestrictedUser;
+        private SerializedProperty _maxAdClient;
+        private SerializedProperty _maxBannerVariable;
+        private SerializedProperty _maxInterVariable;
+        private SerializedProperty _maxRewardVariable;
+        private SerializedProperty _maxRewardInterVariable;
+        private SerializedProperty _maxAppOpenVariable;
+
+        private SerializedProperty _admobAdClient;
+        private SerializedProperty _admobBannerVariable;
+        private SerializedProperty _admobInterVariable;
+        private SerializedProperty _admobRewardVariable;
+        private SerializedProperty _admobRewardInterVariable;
+        private SerializedProperty _admobAppOpenVariable;
+        private SerializedProperty _admobEnableTestMode;
+        private SerializedProperty _admobDevicesTest;
+
+        private void OnEnable()
+        {
+            _adSetting = target as AdSetting;
+            _adNetwork = serializedObject.FindProperty("adNetwork");
+            _adCheckingInterval = serializedObject.FindProperty("adCheckingInterval");
+            _adLoadingInterval = serializedObject.FindProperty("adLoadingInterval");
+
+            _sdkKey = serializedObject.FindProperty("sdkKey");
+            _applovinEnableAgeRestrictedUser = serializedObject.FindProperty("applovinEnableAgeRestrictedUser");
+            _maxAdClient = serializedObject.FindProperty("maxAdClient");
+            _maxBannerVariable = serializedObject.FindProperty("maxBannerVariable");
+            _maxInterVariable = serializedObject.FindProperty("maxInterVariable");
+            _maxRewardVariable = serializedObject.FindProperty("maxRewardVariable");
+            _maxRewardInterVariable = serializedObject.FindProperty("maxRewardInterVariable");
+            _maxAppOpenVariable = serializedObject.FindProperty("maxAppOpenVariable");
+
+            _admobAdClient = serializedObject.FindProperty("admobAdClient");
+            _admobBannerVariable = serializedObject.FindProperty("admobBannerVariable");
+            _admobInterVariable = serializedObject.FindProperty("admobInterVariable");
+            _admobRewardVariable = serializedObject.FindProperty("admobRewardVariable");
+            _admobRewardInterVariable = serializedObject.FindProperty("admobRewardInterVariable");
+            _admobAppOpenVariable = serializedObject.FindProperty("admobAppOpenVariable");
+            _admobEnableTestMode = serializedObject.FindProperty("admobEnableTestMode");
+            _admobDevicesTest = serializedObject.FindProperty("admobDevicesTest");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            // base.OnInspectorGUI();
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(_adCheckingInterval);
+            EditorGUILayout.PropertyField(_adLoadingInterval);
+            EditorGUILayout.PropertyField(_adNetwork);
+
+            SetupMax();
+            SetupAdmob();
+            SetUpButton();
+
+            serializedObject.ApplyModifiedProperties();
+            serializedObject.Update();
+        }
+
+        void SetupMax()
+        {
+            if (_adNetwork.enumValueIndex == (int)AdNetwork.Applovin)
+            {
+                EditorGUILayout.PropertyField(_sdkKey);
+                EditorGUILayout.PropertyField(_applovinEnableAgeRestrictedUser);
+                EditorGUILayout.PropertyField(_maxAdClient);
+                EditorGUILayout.PropertyField(_maxBannerVariable);
+                EditorGUILayout.PropertyField(_maxInterVariable);
+                EditorGUILayout.PropertyField(_maxRewardVariable);
+                EditorGUILayout.PropertyField(_maxRewardInterVariable);
+                EditorGUILayout.PropertyField(_maxAppOpenVariable);
+            }
+        }
+
+        void SetupAdmob()
+        {
+            if (_adNetwork.enumValueIndex == (int)AdNetwork.Admob)
+            {
+                EditorGUILayout.PropertyField(_admobAdClient);
+                EditorGUILayout.PropertyField(_admobBannerVariable);
+                EditorGUILayout.PropertyField(_admobInterVariable);
+                EditorGUILayout.PropertyField(_admobRewardVariable);
+                EditorGUILayout.PropertyField(_admobRewardInterVariable);
+                EditorGUILayout.PropertyField(_admobAppOpenVariable);
+                EditorGUILayout.PropertyField(_admobEnableTestMode);
+                EditorGUILayout.PropertyField(_admobDevicesTest);
+            }
+        }
+
+        void SetUpButton()
+        {
+            if (_adNetwork.enumValueIndex == (int)AdNetwork.Applovin && GUILayout.Button("Create MaxClient And MaxVariable"))
+            {
+                _adSetting.CreateMax();
+            }
+
+            if (_adNetwork.enumValueIndex == (int)AdNetwork.Admob && GUILayout.Button("Create AdmobClient And AdmobVariable"))
+            {
+                _adSetting.CreateAdmob();
+            }
+        }
+    }
+}
+#endif
---
+++ VirtueSky/Advertising/Runtime/General/AdSetting.cs
@@ -0,0 +1,127 @@
+using System.Collections.Generic;
+using UnityEngine;
+using VirtueSky.Attributes;
+using VirtueSky.UtilsEditor;
+
+namespace VirtueSky.Ads
+{
+    public class AdSetting : ScriptableObject
+    {
+        // [SerializeField] private bool autoInit = true;
+        [Range(5, 100), SerializeField] private float adCheckingInterval = 8f;
+        [Range(5, 100), SerializeField] private float adLoadingInterval = 15f;
+        [SerializeField] private AdNetwork adNetwork = AdNetwork.Applovin;
+
+        //  public bool AutoInit => autoInit;
+        public float AdCheckingInterval => adCheckingInterval;
+        public float AdLoadingInterval => adLoadingInterval;
+
+        public AdNetwork CurrentAdNetwork
+        {
+            get => adNetwork;
+            set => adNetwork = value;
+        }
+
+        #region AppLovin
+
+        private const string pathMax = "/Ads/Applovin";
+
+        [Header("Applovin")] [Space, TextArea, SerializeField]
+        private string sdkKey;
+
+        [SerializeField] private bool applovinEnableAgeRestrictedUser;
+        [SerializeField] private MaxAdClient maxAdClient;
+        [SerializeField] private MaxBannerVariable maxBannerVariable;
+        [SerializeField] private MaxInterVariable maxInterVariable;
+        [SerializeField] private MaxRewardVariable maxRewardVariable;
+        [SerializeField] private MaxRewardInterVariable maxRewardInterVariable;
+        [SerializeField] private MaxAppOpenVariable maxAppOpenVariable;
+
+        public string SdkKey => sdkKey;
+        public bool ApplovinEnableAgeRestrictedUser => applovinEnableAgeRestrictedUser;
+        public MaxAdClient MaxAdClient => maxAdClient;
+        public MaxBannerVariable MaxBannerVariable => maxBannerVariable;
+        public MaxInterVariable MaxInterVariable => maxInterVariable;
+        public MaxRewardVariable MaxRewardVariable => maxRewardVariable;
+        public MaxRewardInterVariable MaxRewardInterVariable => maxRewardInterVariable;
+        public MaxAppOpenVariable MaxAppOpenVariable => maxAppOpenVariable;
+
+
+#if UNITY_EDITOR
+        public void CreateMax()
+        {
+            maxAdClient = CreateAsset.CreateAndGetScriptableAsset<MaxAdClient>(pathMax);
+            maxBannerVariable = CreateAsset.CreateAndGetScriptableAsset<MaxBannerVariable>(pathMax);
+            maxInterVariable = CreateAsset.CreateAndGetScriptableAsset<MaxInterVariable>(pathMax);
+            maxRewardVariable = CreateAsset.CreateAndGetScriptableAsset<MaxRewardVariable>(pathMax);
+            maxRewardInterVariable = CreateAsset.CreateAndGetScriptableAsset<MaxRewardInterVariable>(pathMax);
+            maxAppOpenVariable = CreateAsset.CreateAndGetScriptableAsset<MaxAppOpenVariable>(pathMax);
+        }
+#endif
+
+        #endregion
+
+        #region Admob
+
+        private const string pathAdmob = "/Ads/Admob";
+
+        [Header("Admob")] [Space, SerializeField]
+        private AdmobAdClient admobAdClient;
+
+        [SerializeField] private AdmobBannerVariable admobBannerVariable;
+        [SerializeField] private AdmobInterVariable admobInterVariable;
+        [SerializeField] private AdmobRewardVariable admobRewardVariable;
+        [SerializeField] private AdmobRewardInterVariable admobRewardInterVariable;
+        [SerializeField] private AdmobAppOpenVariable admobAppOpenVariable;
+        [SerializeField] private bool admobEnableTestMode;
+        [SerializeField] private List<string> admobDevicesTest;
+        public AdmobAdClient AdmobAdClient => admobAdClient;
+        public AdmobBannerVariable AdmobBannerVariable => admobBannerVariable;
+        public AdmobInterVariable AdmobInterVariable => admobInterVariable;
+        public AdmobRewardVariable AdmobRewardVariable => admobRewardVariable;
+        public AdmobRewardInterVariable AdmobRewardInterVariable => admobRewardInterVariable;
+        public AdmobAppOpenVariable AdmobAppOpenVariable => admobAppOpenVariable;
+        public bool AdmobEnableTestMode => admobEnableTestMode;
+        public List<string> AdmobDevicesTest => admobDevicesTest;
+#if UNITY_EDITOR
+        public void CreateAdmob()
+        {
+            admobAdClient = CreateAsset.CreateAndGetScriptableAsset<AdmobAdClient>(pathAdmob);
+            admobBannerVariable = CreateAsset.CreateAndGetScriptableAsset<AdmobBannerVariable>(pathAdmob);
+            admobInterVariable = CreateAsset.CreateAndGetScriptableAsset<AdmobInterVariable>(pathAdmob);
+            admobRewardVariable = CreateAsset.CreateAndGetScriptableAsset<AdmobRewardVariable>(pathAdmob);
+            admobRewardInterVariable =
+                CreateAsset.CreateAndGetScriptableAsset<AdmobRewardInterVariable>(pathAdmob);
+            admobAppOpenVariable = CreateAsset.CreateAndGetScriptableAsset<AdmobAppOpenVariable>(pathAdmob);
+        }
+#endif
+
+        #endregion
+    }
+
+    public enum AdNetwork
+    {
+        Applovin,
+        Admob
+    }
+
+    public enum BannerPosition
+    {
+        Top = 1,
+        Bottom = 0,
+        TopLeft = 2,
+        TopRight = 3,
+        BottomLeft = 4,
+        BottomRight = 5,
+    }
+
+    public enum BannerSize
+    {
+        Banner = 0, // 320x50
+        Adaptive = 5, // full width
+        MediumRectangle = 1, // 300x250
+        IABBanner = 2, // 468x60
+        Leaderboard = 3, // 728x90
+        //    SmartBanner = 4,
+    }
+}
