d46f66dded746cc86fc6ba059e8310f201047a91
DucNV <ducnv.cntt0502@gmail.com>
2023-11-17 15:31:46 +0700

    add title attribute

---
+++ VirtueSky/Attributes/Core/Enum/CustomColor.cs
@@ -0,0 +1,37 @@
+﻿namespace VirtueSky.Attributes
+{
+    public enum CustomColor
+    {
+        Aqua,
+        Beige,
+        Black,
+        Blue,
+        BlueVariant,
+        DarkBlue,
+        Bright,
+        Brown,
+        Cyan,
+        DarkGray,
+        Fuchsia,
+        Gray,
+        Green,
+        Indigo,
+        LightGray,
+        Lime,
+        Navy,
+        Olive,
+        DarkOlive,
+        Orange,
+        OrangeVariant,
+        Pink,
+        Red,
+        LightRed,
+        RedVariant,
+        DarkRed,
+        Tan,
+        Teal,
+        Violet,
+        White,
+        Yellow
+    }
+}
