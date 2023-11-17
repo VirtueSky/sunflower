90a962364ec75839961eff5e2ec503b6626e5e87
DucNV <ducnv.cntt0502@gmail.com>
2023-11-17 16:16:28 +0700

    add search enum

---
+++ VirtueSky/Attributes/Core/Attribute/EnumAttribue/SearchableEnumAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace VirtueSky.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SearchableEnumAttribute : PropertyAttribute
+    {
+    }
+}
