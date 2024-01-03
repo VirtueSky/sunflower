c8580236e8790d9c1b17100bd5e0efd947997af2
DucNV <ducnv.cntt0502@gmail.com>
2024-01-03 16:27:38 +0700

    Update IapWindowEditor.cs

--- VirtueSky/Iap/Editor/IapWindowEditor.cs
+++ VirtueSky/Iap/Editor/IapWindowEditor.cs
@@ -1,5 +1,7 @@
 ﻿#if UNITY_EDITOR
+using System;
 using UnityEditor;
+using UnityEngine;
 using VirtueSky.UtilsEditor;
 
 namespace VirtueSky.Iap
@@ -7,13 +9,49 @@ namespace VirtueSky.Iap
     public class IapWindowEditor : EditorWindow
     {
 #if VIRTUESKY_IAP
-        [MenuItem("Sunflower/Iap/IapSetting %W", false)]
+        private IapSetting _iapSetting;
+        private Vector2 _scrollPosition;
+        private Editor _editor;
+
+        [MenuItem("Sunflower/Iap/IapSetting %#W", false)]
         public static void MenuOpenAdSettings()
         {
             var iapSetting = CreateAsset.CreateAndGetScriptableAsset<VirtueSky.Iap.IapSetting>("/Iap");
-            Selection.activeObject = iapSetting;
-            EditorUtility.FocusProjectWindow();
+            IapWindowEditor window = GetWindow<IapWindowEditor>("Iap Settings");
+            window._iapSetting = iapSetting;
+            if (window == null)
+            {
+                Debug.LogError("Couldn't open the iap settings window!");
+                return;
+            }
+
+            window.minSize = new Vector2(275, 0);
+            window.Show();
+            // Selection.activeObject = iapSetting;
+            // EditorUtility.FocusProjectWindow();
         }
+
+        private void OnGUI()
+        {
+            if (_editor == null)
+            {
+                _editor = Editor.CreateEditor(_iapSetting);
+            }
+
+            if (_editor == null)
+            {
+                EditorGUILayout.HelpBox("Couldn't create the settings resources editor.", MessageType.Error);
+                return;
+            }
+
+            _editor.DrawHeader();
+            _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+            EditorGUILayout.BeginVertical(new GUIStyle { padding = new RectOffset(6, 3, 3, 3) });
+            _editor.OnInspectorGUI();
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.EndScrollView();
+        }
+
 
         [MenuItem("Sunflower/Iap/Iap Data Variable")]
         public static void CreateVariableTransform()
