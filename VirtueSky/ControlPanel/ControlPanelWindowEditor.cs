ba0e4734c348fa6678af2905dab00b79854bb510
DucNV <ducnv.cntt0502@gmail.com>
2024-01-04 16:06:15 +0700

    Update ControlPanelWindowEditor.cs

--- VirtueSky/ControlPanel/ControlPanelWindowEditor.cs
+++ VirtueSky/ControlPanel/ControlPanelWindowEditor.cs
@@ -253,17 +253,32 @@ namespace VirtueSky.ControlPanel
             GUILayout.Space(10);
             if (GUILayout.Button("Open AdSetting (Alt+2 / Option+2)"))
             {
+#if VIRTUESKY_IAP
                 IapWindowEditor.OpenIapSettingsWindows();
+
+#else
+                Debug.LogError("Add scripting define symbols ( VIRTUESKY_IAP ) to use IAP");
+#endif
             }
 
             if (GUILayout.Button("Create Iap Purchase Product Event"))
             {
+#if VIRTUESKY_IAP
                 IapWindowEditor.CreateIapProductEvent();
+
+#else
+                Debug.LogError("Add scripting define symbols ( VIRTUESKY_IAP ) to use IAP");
+#endif
             }
 
             if (GUILayout.Button("Create Iap Purchase Product Event"))
             {
+#if VIRTUESKY_IAP
                 IapWindowEditor.CreateIsPurchaseProductEvent();
+
+#else
+                Debug.LogError("Add scripting define symbols ( VIRTUESKY_IAP ) to use IAP");
+#endif
             }
 
             GUILayout.EndVertical();
