dc0fac8a2488128d84befd43a0ea1521e6f1a0a2
DucNV <ducnv.cntt0502@gmail.com>
2024-02-05 17:32:02 +0700

    Update EditorScriptDefineSymbols.cs

--- VirtueSky/Utils/Editor/EditorScriptDefineSymbols.cs
+++ VirtueSky/Utils/Editor/EditorScriptDefineSymbols.cs
@@ -6,123 +6,99 @@ namespace VirtueSky.UtilsEditor
 {
     public class EditorScriptDefineSymbols : EditorWindow
     {
+        private const string defaultMenuPath = "Sunflower/ScriptDefineSymbols/";
+
         #region Ads
 
-        private const string menuPathAds = "Sunflower/ScriptDefineSymbols/VIRTUESKY_ADS";
+        private const string menuPathAds = defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_ADS;
 
         [MenuItem(menuPathAds)]
         public static void AdsConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_ADS");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_ADS);
         }
 
         [MenuItem(menuPathAds, true)]
         public static bool IsAdsConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathAds, IsFlagEnabled("VIRTUESKY_ADS"));
+            Menu.SetChecked(menuPathAds, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_ADS));
             return true;
-        }
-
-        public static bool IsAdsFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_ADS");
         }
 
         #endregion
 
         #region Applovin
 
-        private const string menuPathApplovin = "Sunflower/ScriptDefineSymbols/ADS_APPLOVIN";
+        private const string menuPathApplovin = defaultMenuPath + ConstantDefineSymbols.ADS_APPLOVIN;
 
         [MenuItem(menuPathApplovin)]
         public static void ApplovinConfigFlag()
         {
-            SwitchFlag("ADS_APPLOVIN");
+            SwitchFlag(ConstantDefineSymbols.ADS_APPLOVIN);
         }
 
         [MenuItem(menuPathApplovin, true)]
         public static bool IsApplovinConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathApplovin, IsFlagEnabled("ADS_APPLOVIN"));
+            Menu.SetChecked(menuPathApplovin, IsFlagEnabled(ConstantDefineSymbols.ADS_APPLOVIN));
             return true;
-        }
-
-        public static bool IsApplovinFlag()
-        {
-            return IsFlagEnabled("ADS_APPLOVIN");
         }
 
         #endregion
 
         #region Admob
 
-        private const string menuPathAdmob = "Sunflower/ScriptDefineSymbols/ADS_ADMOB";
+        private const string menuPathAdmob = defaultMenuPath + ConstantDefineSymbols.ADS_ADMOB;
 
         [MenuItem(menuPathAdmob)]
         public static void AdmobConfigFlag()
         {
-            SwitchFlag("ADS_ADMOB");
+            SwitchFlag(ConstantDefineSymbols.ADS_ADMOB);
         }
 
         [MenuItem(menuPathAdmob, true)]
         public static bool IsAdmobConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathAdmob, IsFlagEnabled("ADS_ADMOB"));
+            Menu.SetChecked(menuPathAdmob, IsFlagEnabled(ConstantDefineSymbols.ADS_ADMOB));
             return true;
-        }
-
-        public static bool IsAdmobFlag()
-        {
-            return IsFlagEnabled("ADS_ADMOB");
         }
 
         #endregion
 
         #region Adjust
 
-        private const string menuPathAdjust = "Sunflower/ScriptDefineSymbols/VIRTUESKY_ADJUST";
+        private const string menuPathAdjust = defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_ADJUST;
 
         [MenuItem(menuPathAdjust)]
         public static void AdjustConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_ADJUST");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_ADJUST);
         }
 
         [MenuItem(menuPathAdjust, true)]
         public static bool IsAdjustConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathAdjust, IsFlagEnabled("VIRTUESKY_ADJUST"));
+            Menu.SetChecked(menuPathAdjust, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_ADJUST));
             return true;
-        }
-
-        public static bool IsAdjustFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_ADJUST");
         }
 
         #endregion
 
         #region Firebase Analytics
 
-        private const string menuPathAnalytic =
-            "Sunflower/ScriptDefineSymbols/VIRTUESKY_FIREBASE_ANALYTIC";
+        private const string menuPathAnalytic = defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_FIREBASE_ANALYTIC;
 
         [MenuItem(menuPathAnalytic)]
         public static void AnalyticConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_FIREBASE_ANALYTIC");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_FIREBASE_ANALYTIC);
         }
 
         [MenuItem(menuPathAnalytic, true)]
         public static bool IsAnalyticConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathAnalytic, IsFlagEnabled("VIRTUESKY_FIREBASE_ANALYTIC"));
+            Menu.SetChecked(menuPathAnalytic, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_FIREBASE_ANALYTIC));
             return true;
-        }
-
-        public static bool IsAnalyticFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_FIREBASE_ANALYTIC");
         }
 
         #endregion
@@ -130,147 +106,114 @@ namespace VirtueSky.UtilsEditor
         #region Firebase Remote Config
 
         private const string menuPathRemoteConfig =
-            "Sunflower/ScriptDefineSymbols/VIRTUESKY_FIREBASE_REMOTECONFIG";
+            defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_FIREBASE_REMOTECONFIG;
 
         [MenuItem(menuPathRemoteConfig)]
         public static void RemoteConfigConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_FIREBASE_REMOTECONFIG");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_FIREBASE_REMOTECONFIG);
         }
 
         [MenuItem(menuPathRemoteConfig, true)]
         public static bool IsRemoteConfigConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathRemoteConfig, IsFlagEnabled("VIRTUESKY_FIREBASE_REMOTECONFIG"));
+            Menu.SetChecked(menuPathRemoteConfig, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_FIREBASE_REMOTECONFIG));
             return true;
-        }
-
-        public static bool IsRemoteConfigConfigFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_FIREBASE_REMOTECONFIG");
         }
 
         #endregion
 
         #region Firebase App
 
-        private const string menuPathFirebaseApp =
-            "Sunflower/ScriptDefineSymbols/VIRTUESKY_FIREBASE";
+        private const string menuPathFirebaseApp = defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_FIREBASE;
 
         [MenuItem(menuPathFirebaseApp)]
         public static void FirebaseAppConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_FIREBASE");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_FIREBASE);
         }
 
         [MenuItem(menuPathFirebaseApp, true)]
         public static bool IsFirebaseAppConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathFirebaseApp, IsFlagEnabled("VIRTUESKY_FIREBASE"));
+            Menu.SetChecked(menuPathFirebaseApp, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_FIREBASE));
             return true;
-        }
-
-        public static bool IsFirebaseAppFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_FIREBASE");
         }
 
         #endregion
 
         #region Iap
 
-        private const string menuPathIAP = "Sunflower/ScriptDefineSymbols/VIRTUESKY_IAP";
+        private const string menuPathIAP = defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_IAP;
 
         [MenuItem(menuPathIAP)]
         public static void IapConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_IAP");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_IAP);
         }
 
         [MenuItem(menuPathIAP, true)]
         public static bool IsIapFlagEnable()
         {
-            Menu.SetChecked(menuPathIAP, IsFlagEnabled("VIRTUESKY_IAP"));
+            Menu.SetChecked(menuPathIAP, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_IAP));
             return true;
-        }
-
-        public static bool IsIapFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_IAP");
         }
 
         #endregion
 
         #region Ratting
 
-        private const string menuPathRatting = "Sunflower/ScriptDefineSymbols/VIRTUESKY_RATING";
+        private const string menuPathRatting = defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_RATING;
 
         [MenuItem(menuPathRatting)]
         public static void RattingConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_RATING");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_RATING);
         }
 
         [MenuItem(menuPathRatting, true)]
         public static bool IsRattingConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathRatting, IsFlagEnabled("VIRTUESKY_RATING"));
+            Menu.SetChecked(menuPathRatting, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_RATING));
             return true;
-        }
-
-        public static bool IsRattingFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_RATING");
         }
 
         #endregion
 
         #region Notification
 
-        private const string menuPathNotification =
-            "Sunflower/ScriptDefineSymbols/VIRTUESKY_NOTIFICATION";
+        private const string menuPathNotification = defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_NOTIFICATION;
 
         [MenuItem(menuPathNotification)]
         public static void NotificationConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_NOTIFICATION");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_NOTIFICATION);
         }
 
         [MenuItem(menuPathNotification, true)]
         public static bool IsNotificationConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathNotification, IsFlagEnabled("VIRTUESKY_NOTIFICATION"));
+            Menu.SetChecked(menuPathNotification, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_NOTIFICATION));
             return true;
-        }
-
-        public static bool IsNotificationFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_NOTIFICATION");
         }
 
         #endregion
 
         #region AppsFlyer
 
-        private const string menuPathAppsFlyer =
-            "Sunflower/ScriptDefineSymbols/VIRTUESKY_APPSFLYER";
+        private const string menuPathAppsFlyer = defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_APPSFLYER;
 
         [MenuItem(menuPathAppsFlyer)]
         public static void AppsFlyerConfigFlag()
         {
-            SwitchFlag("VIRTUESKY_APPSFLYER");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_APPSFLYER);
         }
 
         [MenuItem(menuPathAppsFlyer, true)]
         public static bool IsAppsFlyerConfigFlagEnable()
         {
-            Menu.SetChecked(menuPathAppsFlyer, IsFlagEnabled("VIRTUESKY_APPSFLYER"));
+            Menu.SetChecked(menuPathAppsFlyer, IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_APPSFLYER));
             return true;
-        }
-
-        public static bool IsAppsFlyerFlag()
-        {
-            return IsFlagEnabled("VIRTUESKY_APPSFLYER");
         }
 
         #endregion
@@ -278,25 +221,62 @@ namespace VirtueSky.UtilsEditor
         #region PRIME_TWEEN_DOTWEEN_ADAPTER
 
         private const string menuPathPrimeTweenDotweenAdapter =
-            "Sunflower/ScriptDefineSymbols/PRIME_TWEEN_DOTWEEN_ADAPTER";
+            defaultMenuPath + ConstantDefineSymbols.PRIME_TWEEN_DOTWEEN_ADAPTER;
 
         [MenuItem(menuPathPrimeTweenDotweenAdapter)]
         public static void PrimeTweenDoTweenAdapterConfigFlag()
         {
-            SwitchFlag("PRIME_TWEEN_DOTWEEN_ADAPTER");
+            SwitchFlag(ConstantDefineSymbols.PRIME_TWEEN_DOTWEEN_ADAPTER);
         }
 
         [MenuItem(menuPathPrimeTweenDotweenAdapter, true)]
         public static bool IsPrimeTweenDoTweenAdapterConfigFlagEnable()
         {
             Menu.SetChecked(menuPathPrimeTweenDotweenAdapter,
-                IsFlagEnabled("PRIME_TWEEN_DOTWEEN_ADAPTER"));
+                IsFlagEnabled(ConstantDefineSymbols.PRIME_TWEEN_DOTWEEN_ADAPTER));
             return true;
         }
 
-        public static bool IsPrimeTweenDoTweenAdapterFlag()
+        #endregion
+
+        #region GPGS
+
+        private const string menuPathGPGS =
+            defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_GPGS;
+
+        [MenuItem(menuPathGPGS)]
+        public static void GPGSConfigFlag()
         {
-            return IsFlagEnabled("PRIME_TWEEN_DOTWEEN_ADAPTER");
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_GPGS);
+        }
+
+        [MenuItem(menuPathGPGS, true)]
+        public static bool IsGPGSConfigFlagEnable()
+        {
+            Menu.SetChecked(menuPathGPGS,
+                IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_GPGS));
+            return true;
+        }
+
+        #endregion
+
+        #region Apple Auth
+
+        private const string menuPathAppleAuth =
+            defaultMenuPath + ConstantDefineSymbols.VIRTUESKY_APPLE_AUTH;
+
+        [MenuItem(menuPathAppleAuth)]
+        public static void AppleAuthConfigFlag()
+        {
+            SwitchFlag(ConstantDefineSymbols.VIRTUESKY_APPLE_AUTH);
+        }
+
+        [MenuItem(menuPathAppleAuth, true)]
+        public static bool IsAppleAuthConfigFlagEnable()
+        {
+            Menu.SetChecked(menuPathAppleAuth,
+                IsFlagEnabled(ConstantDefineSymbols.VIRTUESKY_APPLE_AUTH));
+            return true;
         }
 
         #endregion
