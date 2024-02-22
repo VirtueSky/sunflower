2cf31a73407aa5b90c9f37271b501d228580f243
DucNV <ducnv@gameestudio.com>
2024-02-22 15:33:35 +0700

    Update CPAudioDrawer.cs

--- VirtueSky/ControlPanel/CPAudioDrawer.cs
+++ VirtueSky/ControlPanel/CPAudioDrawer.cs
@@ -12,19 +12,89 @@ namespace VirtueSky.ControlPanel.Editor
             GUILayout.BeginVertical();
             GUILayout.Label("AUDIO", EditorStyles.boldLabel);
             GUILayout.Space(10);
-            if (GUILayout.Button("Create SoundComponentPool"))
+            if (GUILayout.Button("Create Sound Component Pool"))
             {
                 AudioWindowEditor.CreateSoundComponentPool();
-            }
-
-            if (GUILayout.Button("Create Event Audio Handle"))
-            {
-                AudioWindowEditor.CreateEventAudioHandle();
             }
 
             if (GUILayout.Button("Create Sound Data"))
             {
                 AudioWindowEditor.CreateSoundData();
+            }
+
+            GUILayout.Space(10);
+            CPUtility.GuiLine(2);
+            GUILayout.Space(10);
+            GUILayout.Label("MUSIC EVENT", EditorStyles.boldLabel);
+            GUILayout.Space(10);
+            if (GUILayout.Button("Create Play Music Event"))
+            {
+                AudioWindowEditor.CreatePlayMusicEvent();
+            }
+
+            if (GUILayout.Button("Create Pause Music Event"))
+            {
+                AudioWindowEditor.CreatePauseMusicEvent();
+            }
+
+            if (GUILayout.Button("Create Resume Music Event"))
+            {
+                AudioWindowEditor.CreateResumeMusicEvent();
+            }
+
+            if (GUILayout.Button("Create Stop Music Event"))
+            {
+                AudioWindowEditor.CreateStopMusicEvent();
+            }
+
+            GUILayout.Space(10);
+            CPUtility.GuiLine(2);
+            GUILayout.Space(10);
+            GUILayout.Label("SFX EVENT", EditorStyles.boldLabel);
+            GUILayout.Space(10);
+            if (GUILayout.Button("Create Play Sfx Event"))
+            {
+                AudioWindowEditor.CreatePlaySfxEvent();
+            }
+
+            if (GUILayout.Button("Create Pause Sfx Event"))
+            {
+                AudioWindowEditor.CreatePauseSfxEvent();
+            }
+
+            if (GUILayout.Button("Create Resume Sfx Event"))
+            {
+                AudioWindowEditor.CreateResumeSfxEvent();
+            }
+
+            if (GUILayout.Button("Create Finish Sfx Event"))
+            {
+                AudioWindowEditor.CreateFinishSfxEvent();
+            }
+
+            if (GUILayout.Button("Create Stop Sfx Event"))
+            {
+                AudioWindowEditor.CreateStopSfxEvent();
+            }
+
+            if (GUILayout.Button("Create Stop All Sfx Event"))
+            {
+                AudioWindowEditor.CreateStopAllSfxEvent();
+            }
+
+            GUILayout.Space(10);
+            CPUtility.GuiLine(2);
+            GUILayout.Space(10);
+            GUILayout.Label("VOLUME CHANGE", EditorStyles.boldLabel);
+            GUILayout.Space(10);
+            if (GUILayout.Button("Create Music Volume Variable"))
+            {
+                AudioWindowEditor.CreateMusicVolume();
+            }
+
+            if (GUILayout.Button("Create Sfx Volume Variable"))
+            {
+                AudioWindowEditor.CreateSfxVolume();
             }
 
             GUILayout.EndVertical();
