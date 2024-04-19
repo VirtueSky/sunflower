2174efeeca5f43f3798968bde8e8602c9d08ba14
Wolf <ducnv.cntt0502@gmail.com>
2024-04-19 21:26:50 +0700

    - update: version

--- VirtueSky/ControlPanel/ConstantPackage.cs
+++ VirtueSky/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionSunflower = "2.6.4";
+        public const string VersionSunflower = "2.6.5";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "4.10.0";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
