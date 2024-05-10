ef8c96749820900490081db1400bceac7eb008c6
Wolf <ducnv.cntt0502@gmail.com>
2024-05-10 23:58:19 +0700

    -update: version

--- README.md
+++ README.md
@@ -23,9 +23,9 @@
 
 ### Add the line below to `Packages/manifest.json`
 
-for version `2.6.7`
+for version `2.6.8`
 ```csharp
-"com.virtuesky.sunflower":"https://github.com/VirtueSky/sunflower.git#2.6.7",
+"com.virtuesky.sunflower":"https://github.com/VirtueSky/sunflower.git#2.6.8",
 ```
 
 ## Includes modules
--- VirtueSky/ControlPanel/ConstantPackage.cs
+++ VirtueSky/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionSunflower = "2.6.7";
+        public const string VersionSunflower = "2.6.8";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "4.10.0";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
