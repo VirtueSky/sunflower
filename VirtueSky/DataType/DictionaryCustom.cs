c4be5a94b2613e5c04677bc64319743150412342
DucNV <ducnv@gameestudio.com>
2024-07-22 09:41:29 +0700

    Update DictionaryCustom.cs

--- VirtueSky/DataType/DictionaryCustom.cs
+++ VirtueSky/DataType/DictionaryCustom.cs
@@ -12,175 +12,164 @@ namespace VirtueSky.DataType
     {
         [TableList, SerializeField] private List<DictionaryCustomData<TKey, TValue>> dictionaryData;
 
-        [NonSerialized] private Dictionary<TKey, TValue> m_dictionary = new Dictionary<TKey, TValue>();
-        private ICollection _keys;
-        private ICollection _values;
-        private ICollection<TKey> _keys1;
-        private ICollection<TValue> _values1;
-
-        public Dictionary<TKey, TValue> GetDictionary => m_dictionary;
+        [NonSerialized] private Dictionary<TKey, TValue> m_dict = new Dictionary<TKey, TValue>();
 
         public void OnAfterDeserialize()
         {
             if (dictionaryData is { Count: > 0 })
             {
-                if (m_dictionary is { Count: > 0 })
+                if (m_dict is { Count: > 0 })
                 {
-                    m_dictionary.Clear();
+                    m_dict.Clear();
                 }
 
                 foreach (var data in dictionaryData)
                 {
                     if (data.key != null && data.value != null)
                     {
-                        m_dictionary[data.key] = data.value;
+                        m_dict[data.key] = data.value;
                     }
                 }
-
-                //dictionaryData.Clear();
-                // dictionaryData = null;
             }
         }
 
         public void OnBeforeSerialize()
         {
+            UpdateList();
         }
 
-        private void OnValueChanged()
+        private void UpdateList()
         {
-            if (m_dictionary is { Count: > 0 })
+            if (Application.isPlaying)
             {
-                dictionaryData.Clear();
-                foreach (var kvp in m_dictionary)
+                if (m_dict is { Count: > 0 })
                 {
-                    dictionaryData.Add(new DictionaryCustomData<TKey, TValue>(kvp.Key, kvp.Value));
+                    dictionaryData.Clear();
+                    foreach (var kvp in m_dict)
+                    {
+                        dictionaryData.Add(new DictionaryCustomData<TKey, TValue>(kvp.Key, kvp.Value));
+                    }
                 }
             }
         }
 
         public void Add(object key, object value)
         {
-            m_dictionary.Add((TKey)key, (TValue)value);
-            OnValueChanged();
+            m_dict.Add((TKey)key, (TValue)value);
         }
 
         public void Add(KeyValuePair<TKey, TValue> item)
         {
-            m_dictionary.Add(item.Key, item.Value);
-            OnValueChanged();
+            m_dict.Add(item.Key, item.Value);
         }
 
         void ICollection<KeyValuePair<TKey, TValue>>.Clear()
         {
-            m_dictionary.Clear();
-            OnValueChanged();
+            m_dict.Clear();
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
         {
-            return ((IDictionary)m_dictionary).Contains((TKey)item.Key);
+            return ((IDictionary)m_dict).Contains((TKey)item.Key);
         }
 
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
         {
-            ((IDictionary<TKey, TValue>)m_dictionary).CopyTo(array, arrayIndex);
-            OnValueChanged();
+            ((IDictionary<TKey, TValue>)m_dict).CopyTo(array, arrayIndex);
         }
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
-            return ((IDictionary<TKey, TValue>)m_dictionary).Remove(item);
+            return ((IDictionary<TKey, TValue>)m_dict).Remove(item);
         }
 
-        public int Count => m_dictionary.Count;
+        public int Count => m_dict.Count;
 
-        public bool IsReadOnly => ((IDictionary)m_dictionary).IsReadOnly;
+        public bool IsReadOnly => ((IDictionary)m_dict).IsReadOnly;
 
 
         void IDictionary.Clear()
         {
-            ((IDictionary<TKey, TValue>)m_dictionary).Clear();
+            ((IDictionary<TKey, TValue>)m_dict).Clear();
         }
 
         public bool Contains(object key)
         {
-            return ((IDictionary<TKey, TValue>)m_dictionary).Contains((KeyValuePair<TKey, TValue>)key);
+            return ((IDictionary<TKey, TValue>)m_dict).Contains((KeyValuePair<TKey, TValue>)key);
         }
 
         IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
         {
-            return ((IDictionary<TKey, TValue>)m_dictionary).GetEnumerator();
+            return ((IDictionary<TKey, TValue>)m_dict).GetEnumerator();
         }
 
         IDictionaryEnumerator IDictionary.GetEnumerator()
         {
-            return ((IDictionary)m_dictionary).GetEnumerator();
+            return ((IDictionary)m_dict).GetEnumerator();
         }
 
         public void Remove(object key)
         {
-            m_dictionary.Remove((TKey)key);
-            OnValueChanged();
+            m_dict.Remove((TKey)key);
         }
 
-        public bool IsFixedSize => ((IDictionary)m_dictionary).IsFixedSize;
+        public bool IsFixedSize => ((IDictionary)m_dict).IsFixedSize;
 
 
         public object this[object key]
         {
-            get => ((IDictionary)m_dictionary)[key];
-            set => ((IDictionary)m_dictionary)[key] = value;
+            get => ((IDictionary)m_dict)[key];
+            set => ((IDictionary)m_dict)[key] = value;
         }
 
-        ICollection IDictionary.Keys => ((IDictionary)m_dictionary).Keys;
+        ICollection IDictionary.Keys => ((IDictionary)m_dict).Keys;
 
-        ICollection<TValue> IDictionary<TKey, TValue>.Values => (ICollection<TValue>)((IDictionary)m_dictionary).Values;
+        ICollection<TValue> IDictionary<TKey, TValue>.Values => (ICollection<TValue>)((IDictionary)m_dict).Values;
 
-        ICollection<TKey> IDictionary<TKey, TValue>.Keys => (ICollection<TKey>)((IDictionary)m_dictionary).Keys;
+        ICollection<TKey> IDictionary<TKey, TValue>.Keys => (ICollection<TKey>)((IDictionary)m_dict).Keys;
 
-        ICollection IDictionary.Values => ((IDictionary)m_dictionary).Values;
+        ICollection IDictionary.Values => ((IDictionary)m_dict).Values;
 
         public void Add(TKey key, TValue value)
         {
-            m_dictionary.Add(key, value);
-            OnValueChanged();
+            m_dict.Add(key, value);
         }
 
         public bool ContainsKey(TKey key)
         {
-            return m_dictionary.ContainsKey(key);
+            return m_dict.ContainsKey(key);
         }
 
         public bool Remove(TKey key)
         {
-            return m_dictionary.Remove(key);
+            return m_dict.Remove(key);
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
-            return m_dictionary.TryGetValue(key, out value);
+            return m_dict.TryGetValue(key, out value);
         }
 
         public TValue this[TKey key]
         {
-            get => ((IDictionary<TKey, TValue>)m_dictionary)[key];
-            set => ((IDictionary<TKey, TValue>)m_dictionary)[key] = value;
+            get => ((IDictionary<TKey, TValue>)m_dict)[key];
+            set => ((IDictionary<TKey, TValue>)m_dict)[key] = value;
         }
 
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return ((IDictionary)m_dictionary).GetEnumerator();
+            return ((IDictionary)m_dict).GetEnumerator();
         }
 
         public void CopyTo(Array array, int index)
         {
-            ((IDictionary)m_dictionary).CopyTo(array, index);
+            ((IDictionary)m_dict).CopyTo(array, index);
         }
 
 
-        public bool IsSynchronized => ((IDictionary)m_dictionary).IsSynchronized;
-        public object SyncRoot => ((IDictionary)m_dictionary).SyncRoot;
+        public bool IsSynchronized => ((IDictionary)m_dict).IsSynchronized;
+        public object SyncRoot => ((IDictionary)m_dict).SyncRoot;
     }
 
     [Serializable]
