07bb486841624e2009cb02d99d79f61b20381d20
DucNV <ducnv@gameestudio.com>
2024-08-08 11:11:09 +0700

    - update firebase 12.2.0 sdk
    Co-Authored-By: VirtueSky <126542083+VirtueSky@users.noreply.github.com>

--- VirtueSky/ControlPanel/ConstantPackage.cs
+++ VirtueSky/ControlPanel/ConstantPackage.cs
@@ -50,35 +50,35 @@
         public const string PackageNameFirebaseApp = "com.google.firebase.app";
 
         public const string MaxVersionFirebaseApp =
-            "https://github.com/firebase-unity/firebase-app.git#12.1.0";
+            "https://github.com/firebase-unity/firebase-app.git#12.2.0";
 
         public const string PackageNameFirebaseRemoteConfig = "com.google.firebase.remote-config";
 
         public const string MaxVersionFirebaseRemoteConfig =
-            "https://github.com/firebase-unity/firebase-remote-config.git#12.1.0";
+            "https://github.com/firebase-unity/firebase-remote-config.git#12.2.0";
 
         public const string PackageNameFirebaseAnalytics = "com.google.firebase.analytics";
 
         public const string MaxVersionFirebaseAnalytics =
-            "https://github.com/firebase-unity/firebase-analytics.git#12.1.0";
+            "https://github.com/firebase-unity/firebase-analytics.git#12.2.0";
 
         public const string PackageNameFirebaseDatabase = "com.google.firebase.database";
 
         public const string MaxVersionFirebaseDatabase =
-            "https://github.com/firebase-unity/firebase-database.git#12.1.0";
+            "https://github.com/firebase-unity/firebase-database.git#12.2.0";
 
         public const string PackageNameFirebaseAuth = "com.google.firebase.auth";
-        public const string MaxVersionFirebaseAuth = "https://github.com/firebase-unity/firebase-auth.git#12.1.0";
+        public const string MaxVersionFirebaseAuth = "https://github.com/firebase-unity/firebase-auth.git#12.2.0";
 
         public const string PackageNameFirebaseCrashlytics = "com.google.firebase.crashlytics";
 
         public const string MaxVersionFirebaseCrashlytics =
-            "https://github.com/firebase-unity/firebase-crashlytics.git#12.1.0";
+            "https://github.com/firebase-unity/firebase-crashlytics.git#12.2.0";
 
         public const string PackageNameFirebaseSupportIos = "com.google.firebase.support-ios";
 
         public const string MaxVersionFirebaseSupportIos =
-            "https://github.com/firebase-unity/firebase-support-ios.git#12.1.0";
+            "https://github.com/firebase-unity/firebase-support-ios.git#12.2.0";
 
         #endregion
 
