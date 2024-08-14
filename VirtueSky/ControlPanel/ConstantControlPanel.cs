4a9892a3b8e9d44de4bf749c8c19a4f9eb61b70c
DucNV <ducnv@gameestudio.com>
2024-08-14 15:23:58 +0700

    - add scroll view to tab AppsFlyer

--- VirtueSky/ControlPanel/ConstantControlPanel.cs
+++ VirtueSky/ControlPanel/ConstantControlPanel.cs
@@ -3,6 +3,6 @@ namespace VirtueSky.ControlPanel.Editor
     public class ConstantControlPanel
     {
         public static int WIDTH_CONTENT_BUTTON_STATE_CONTROL_PANEL = 210;
-        public static int POSITION_X_START_CONTENT = WIDTH_CONTENT_BUTTON_STATE_CONTROL_PANEL + 10;
+        public static int POSITION_X_START_CONTENT = WIDTH_CONTENT_BUTTON_STATE_CONTROL_PANEL + 5;
     }
 }
