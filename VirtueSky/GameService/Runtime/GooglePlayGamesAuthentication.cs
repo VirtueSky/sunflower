4e27db84cf549cca2a8e98b80a8230cfbeac4b87
DucNV <ducnv@gameestudio.com>
2024-08-16 14:02:22 +0700

    - update: add method GetNewServerCode for GooglePlayGamesAuthentication

--- VirtueSky/GameService/Runtime/GooglePlayGamesAuthentication.cs
+++ VirtueSky/GameService/Runtime/GooglePlayGamesAuthentication.cs
@@ -3,7 +3,9 @@ using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 #endif
 
+using System.Threading.Tasks;
 using UnityEngine;
+using VirtueSky.Events;
 using VirtueSky.Inspector;
 
 namespace VirtueSky.GameService
@@ -11,6 +13,8 @@ namespace VirtueSky.GameService
     [EditorIcon("icon_authentication")]
     public class GooglePlayGamesAuthentication : ServiceAuthentication
     {
+        [SerializeField] private EventNoParam getNewServerCodeEvent;
+
         protected override void Init()
         {
 #if UNITY_ANDROID && VIRTUESKY_GPGS
@@ -18,12 +22,27 @@ namespace VirtueSky.GameService
             status.SetNotLoggedIn();
             PlayGamesPlatform.Activate();
             loginEvent.AddListener(Login);
+            getNewServerCodeEvent.AddListener(OnGetNewServerCode);
 #endif
+        }
+#if UNITY_ANDROID && VIRTUESKY_GPGS
+        private async void OnGetNewServerCode()
+        {
+            if (!PlayGamesPlatform.Instance.IsAuthenticated()) return;
+            (serverCode.Value, status.Value) = await GetNewServerCode();
+        }
+
+        private Task<(string, StatusLogin)> GetNewServerCode()
+        {
+            var taskSource = new TaskCompletionSource<(string, StatusLogin)>();
+
+            PlayGamesPlatform.Instance.RequestServerSideAccess(true,
+                code => taskSource.SetResult((code, StatusLogin.Successful)));
+            return taskSource.Task;
         }
 
         protected override void Login()
         {
-#if UNITY_ANDROID && VIRTUESKY_GPGS
             PlayGamesPlatform.Instance.Authenticate((success) =>
             {
                 if (success == SignInStatus.Success)
@@ -60,9 +79,8 @@ namespace VirtueSky.GameService
                     });
                 }
             });
-#endif
         }
-
+#endif
         public static bool IsSignIn()
         {
 #if UNITY_ANDROID && VIRTUESKY_GPGS
