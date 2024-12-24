14e62f8776c0a4eec4e8046fc2510818aa7eb12c
DucNV <ducnv@gameestudio.com>
2024-12-24 15:56:51 +0700

    - update package
    Co-Authored-By: VirtueSky <126542083+VirtueSky@users.noreply.github.com>

--- VirtueSky/ControlPanel/ConstantPackage.cs
+++ VirtueSky/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionSunflower = "3.2.4";
+        public const string VersionSunflower = "3.2.5";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "4.12.2";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
@@ -107,8 +107,6 @@
         public const string PackageNameAppleSignIn = "com.lupidan.apple-signin-unity";
 
         public const string MaxVersionAppleSignIn = "https://github.com/lupidan/apple-signin-unity.git#v1.4.3";
-        //public const string PackageNameAnimancer = "com.virtuesky.animancer";
-        //public const string MaxVersionAnimancer = "https://github.com/VirtueSky/animancer-unity.git#7.0.0";
 
         #region Spine
 
