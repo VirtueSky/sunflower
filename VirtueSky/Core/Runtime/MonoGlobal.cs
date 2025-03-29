fdfc0530e7382c1e306d3f949de4e3b8eb851844
Ducnv <ducnv.cntt0502@gmail.com>
2025-03-29 22:51:17 +0700

    - fix delay handle
    - use for instead of foreach
    - remove _timers.AddRange(_timersToAdd);

--- VirtueSky/Core/Runtime/MonoGlobal.cs
+++ VirtueSky/Core/Runtime/MonoGlobal.cs
@@ -161,9 +161,9 @@ namespace VirtueSky.Core
 
         internal void CancelAllDelayHandle()
         {
-            foreach (var timer in _timers)
+            for (var i = 0; i < _timers.Count; i++)
             {
-                timer.Cancel();
+                _timers[i].Cancel();
             }
 
             _timers = new List<DelayHandle>();
@@ -172,17 +172,17 @@ namespace VirtueSky.Core
 
         internal void PauseAllDelayHandle()
         {
-            foreach (var timer in _timers)
+            for (var i = 0; i < _timers.Count; i++)
             {
-                timer.Pause();
+                _timers[i].Pause();
             }
         }
 
         internal void ResumeAllDelayHandle()
         {
-            foreach (var timer in _timers)
+            for (var i = 0; i < _timers.Count; i++)
             {
-                timer.Resume();
+                _timers[i].Resume();
             }
         }
 
@@ -190,13 +190,17 @@ namespace VirtueSky.Core
         {
             if (_timersToAdd.Count > 0)
             {
-                _timers.AddRange(_timersToAdd);
+                for (var i = 0; i < _timersToAdd.Count; i++)
+                {
+                    _timers.Add(_timersToAdd[i]);
+                }
+
                 _timersToAdd.Clear();
             }
 
-            foreach (var timer in _timers)
+            for (var i = 0; i < _timers.Count; i++)
             {
-                timer.Update();
+                _timers[i].Update();
             }
 
             _timers.RemoveAll(t => t.IsDone);
