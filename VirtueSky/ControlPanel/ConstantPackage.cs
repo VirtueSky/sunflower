ea329bfcc30728fc50a9e8ffcd6085c7f17ffd6a
VirtueSky <virtuesky.0520@gmail.com>
2025-09-29 15:06:21 +0700

    - update version

--- VirtueSky/ControlPanel/ConstantPackage.cs
+++ VirtueSky/ControlPanel/ConstantPackage.cs
@@ -2,7 +2,7 @@
 {
     public class ConstantPackage
     {
-        public const string VersionSunflower = "3.4.7";
+        public const string VersionSunflower = "3.4.8";
         public const string PackageNameInAppPurchase = "com.unity.purchasing";
         public const string MaxVersionInAppPurchase = "5.0.1";
         public const string PackageNameNewtonsoftJson = "com.unity.nuget.newtonsoft-json";
