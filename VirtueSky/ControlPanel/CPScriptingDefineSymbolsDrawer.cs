0825a0f007f65dc978612053aabdb508a9e9114e
VirtueSky <duc.nguyenvan@inwave.vn>
2026-08-20 17:03:22 +0700

    update VLog

--- VirtueSky/ControlPanel/CPScriptingDefineSymbolsDrawer.cs
+++ VirtueSky/ControlPanel/CPScriptingDefineSymbolsDrawer.cs
@@ -38,7 +38,7 @@ namespace VirtueSky.ControlPanel.Editor
             CPUtility.DrawButtonAddDefineSymbols(ConstantDefineSymbols.UNITASK_TEXTMESHPRO_SUPPORT);
             CPUtility.DrawButtonAddDefineSymbols(ConstantDefineSymbols.VIRTUESKY_BAKINGSHEET);
             CPUtility.DrawButtonAddDefineSymbols(ConstantDefineSymbols.VIRTUESKY_UNITY_SERVICES);
-            CPUtility.DrawButtonAddDefineSymbols(ConstantDefineSymbols.VIRTUESKU_DEBUG_LOG);
+            CPUtility.DrawButtonAddDefineSymbols(ConstantDefineSymbols.VIRTUESKY_DEBUG_LOG);
             EditorGUILayout.EndScrollView();
             GUILayout.Space(10);
             GUILayout.EndVertical();
--- VirtueSky/Utils/Editor/ConstantDefineSymbols.cs
+++ VirtueSky/Utils/Editor/ConstantDefineSymbols.cs
@@ -26,6 +26,6 @@ namespace VirtueSky.UtilsEditor
         public const string VIRTUESKY_BAKINGSHEET = "VIRTUESKY_BAKINGSHEET";
         public const string VIRTUESKY_UNITY_SERVICES = "VIRTUESKY_UNITY_SERVICES";
         public const string ASSET_FINDER_ADDRESSABLE = "AssetFinderADDRESSABLE";
-        public const string VIRTUESKU_DEBUG_LOG = "VIRTUESKU_DEBUG_LOG";
+        public const string VIRTUESKY_DEBUG_LOG = "VIRTUESKY_DEBUG_LOG";
     }
 }
